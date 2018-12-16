228c901966e167be300f0280dc71d2f75dc82cb7
shigeru <matsuzaki215@gmail.com>
2018-12-16 20:22:35 +0900

    add setting for pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
 """The setup script."""
 
 import re
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
 from os import path
 
+<<<<<<< Updated upstream
 package_name = 'bqlint'
 
 root_dir = path.abspath(path.dirname(__file__))
+=======
+# The directory containing this file
+root_dir = pathlib.Path(__file__).parent
+>>>>>>> Stashed changes
 
 
 def _requirements():
     return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
 
 
+<<<<<<< Updated upstream
+=======
+try:
+    with open(path.join(root_dir, 'README.rst')) as f:
+        long_description = f.read()
+except IOError:
+    long_description = ''
+
+>>>>>>> Stashed changes
 # read __init__
 with open(path.join(root_dir, package_name, '__init__.py')) as f:
     init_text = f.read()
@@ -25,9 +40,16 @@ setup(
     # license=_license,
     author='shigeru',
     author_email='matsuzaki215@gmail.com',
+<<<<<<< Updated upstream
     url='https://github.com/shigeru0215/bqlint',
     description='ref: https://github.com/shigeru0215/bqlint',
     packages=[package_name],
+=======
+    url='https://github.com/shigeru0215/sqlint',
+    description='Simple Sql Linter',
+    long_description=long_description,
+    packages=find_packages(exclude=("tests",)),
+>>>>>>> Stashed changes
     test_suite='tests',
     install_requires=_requirements(),
     classifiers=[
